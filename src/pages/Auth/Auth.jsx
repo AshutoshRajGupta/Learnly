6cfea02b37c74a6ae49b7439c9928553d5e2ac1f
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-07-29 20:27:18 +0530

    almost done

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,16 @@
+export const Auth = ({ signUp }) => {
+  return (
+    <div className={`${signUp && "md:mt-32"}`}>
+      <p className="text-3xl md:text-5xl text-gray-600 font-semibold text-center">
+        The New Social
+      </p>
+      <p className="text-xl md:text-3xl text-gray-500 text-center">
+        Communities on{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-red-400  italic font-semibold">
+          Learnly...
+        </span>{" "}
+        bring people together
+      </p>
+    </div>
+  );
+};
